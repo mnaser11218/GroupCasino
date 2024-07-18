7d9e9a9cd36b33d25e3a670bb4fe0e6922ef2b4a
Josue <josue@zipcodersmbp10.lan>
2024-07-17 22:05:48 -0400

    BlackJackProgress

--- src/main/java/com/github/zipcodewilmington/casino/games/blackjack/BlackJackGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/blackjack/BlackJackGame.java
@@ -1,15 +1,54 @@
 package com.github.zipcodewilmington.casino.games.blackjack;
 
+import com.github.zipcodewilmington.casino.Card;
+import com.github.zipcodewilmington.casino.CardDeck;
 import com.github.zipcodewilmington.casino.GamblingGameInterface;
 import com.github.zipcodewilmington.casino.PlayerInterface;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class BlackJackGame implements GamblingGameInterface {
 
+    static Scanner scanner = new Scanner(System.in);
+    static CardDeck deck = new CardDeck();
+    private static ArrayList<Card> playerHand = new ArrayList<>();
+    private static ArrayList<Card> dealerHand = new ArrayList<>();
+    static boolean turnIsRunning = true;
+    static int totalOfPlayerHand = 0;
+    static int totalOfDealerHand = 0;
 
 
-    public void initializeHands(){
+
+    public static void initializeHands(){
+        System.out.println("\nDealer Hand:");
+        dealerHand.add(deck.dealACard());
+        dealerHand.add(deck.dealACard());
+        displayDealerHand();
+
+        System.out.println("\nYour Hand:");
+        playerHand.add(deck.dealACard());
+        playerHand.add(deck.dealACard());
+        displayPlayerHand();
+    }
+
+    public static void displayPlayerHand(){
+        totalOfPlayerHand = 0;
+        for (Card x : playerHand){
+            System.out.println(x.getCardValue() + " of " + x.getSuit());
+            totalOfPlayerHand += x.getCardValue();
+        }
+        System.out.println("Your hand total: " + totalOfPlayerHand);
+    }
+
+
+    public static void displayDealerHand(){
+        totalOfDealerHand = 0;
+        for (Card x : dealerHand){
+            System.out.println(x.getCardValue() + " of " + x.getSuit());
+            totalOfDealerHand += x.getCardValue();
+        }
+        System.out.println("Your hand total: " + totalOfDealerHand);
 
     }
 
@@ -35,23 +74,53 @@ public class BlackJackGame implements GamblingGameInterface {
 
     @Override
     public void run() {
+
         //print blackjack console
         //set bet
-        //initiliazie new game/round
+        //initialize new game/round
         //while isRunning...
         //deal hands
         //check if dealer wins default
         //player turn
         //check dealer
         //confirm next new round or quit
-        //end looop
+        //end loop
 
     }
 
 
-    public void displayBeginning(){
-        System.out.println("****** WELCOME TO BLACK JACK! ******");
+    public static void displayBeginning(){
+        System.out.println("\n****** WELCOME TO BLACK JACK! ******");
         System.out.println("Please place your bet: (Min: $5 / Max $50)");
+        System.out.println("************************************");
+        int userInput = scanner.nextInt();
+        System.out.println("Dealer matches your bet.");
+        System.out.println("Total Pot: " + (userInput * 2));
+    }
+
+    public static void playerTurn(){
+//        turnIsRunning = true;
+        while (turnIsRunning) {
+            if (totalOfPlayerHand > 21){
+                System.out.println("!!! BUST !!!");
+                turnIsRunning = false;
+                break;
+            }
+            System.out.println("\nWould you like to hit or stand?");
+            System.out.println("Press 1): to Hit");
+            System.out.println("Press 2): to Stand");
+            int userInput = scanner.nextInt();
+            if (userInput == 1) {
+                playerHand.add(deck.dealACard());
+                displayPlayerHand();
+            } else if (userInput == 2) {
+                System.out.println("You chose to stand!");
+                turnIsRunning = false;
+            } else {
+                System.out.println("Not an option.");
+            }
+        }
+
     }
 
 
@@ -67,7 +136,11 @@ public class BlackJackGame implements GamblingGameInterface {
     //pLAY ANOTHER HAND(PLAY AGAIN?) y/n or quit
 
 
-
+    public static void main(String[] args) {
+        displayBeginning();
+        initializeHands();
+        playerTurn();
+    }
 
 
 
--- src/test/java/com/github/zipcodewilmington/CardTest.java
+++ src/test/java/com/github/zipcodewilmington/CardTest.java
@@ -11,7 +11,7 @@ public class CardTest {
 
     @Test
     public void getSuitTest(){
-        Card oneCard = new Card(CardValue.TWO, Suit.SPADES);
+        Card oneCard = new Card(2, Suit.SPADES);
         oneCard.setSuit(Suit.HEARTS);
         Suit expected = Suit.HEARTS;
         Suit actual = oneCard.getSuit();
@@ -20,10 +20,10 @@ public class CardTest {
 
     @Test
     public void getCardValueTest(){
-        Card oneCard = new Card(CardValue.TWO, Suit.SPADES);
-        oneCard.setCardValue(CardValue.JACK);
-        CardValue expected = CardValue.JACK;
-        CardValue actual = oneCard.getCardValue();
+        Card oneCard = new Card(2, Suit.SPADES);
+        oneCard.setCardValue(10);
+        int expected = 10;
+        int actual = oneCard.getCardValue();
         Assert.assertEquals(expected,actual);
     }
 
