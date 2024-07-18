6994b4cc8f17c0b36ceccb053aa591145b195ef5
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-07-17 22:29:49 -0400

    finished slots methods

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
@@ -10,6 +10,11 @@ import java.util.Random;
  */
 public class SlotsGame {
 
+    public static void main(String[] args) {
+
+
+    }
+
     public ArrayList<Integer> spinWheel(boolean isLeverPulled, int size) {
         ArrayList<Integer> newArray = new ArrayList<>();
         Random random = new Random();
@@ -24,14 +29,17 @@ public class SlotsGame {
     }
 
     public boolean checkForMatchThree(ArrayList<Integer> matchThree) {
-        int[] count = new int[10];
-
+        //creates an array 'duplicate' from 0-9
+        int[] duplicate = new int[10];
+        //counts occurences of each number in the array 'duplicate'
         for (int number : matchThree) {
-            count[number]++;
+            duplicate[number]++;
         }
-
+        //iterates through the 'duplicate' array if any number has a value of 3 or 4 return true
         for (int i = 0; i < 10; i++) {
-            if (count[i] == 3) {
+            if (duplicate[i] == 3 ) {
+                return true;
+            } else if (duplicate[i] == 4) {
                 return true;
             }
         }
@@ -39,28 +47,42 @@ public class SlotsGame {
     }
 
     public boolean checkForMatchFive(ArrayList<Integer> matchFive) {
-
+        //creates an array 'duplicate' from 0-9
+        int[] duplicate = new int[10];
+        //counts occurences of each number in the array 'duplicate'
+        for (int number : matchFive) {
+            duplicate[number]++;
+        }
+        //iterates through the 'duplicate' array if any number has a value of 5
+        for (int i = 0; i < 10; i++) {
+            if (duplicate[i] == 5) {
+                return true;
+            }
+        }
         return false;
     }
 
     public boolean jackpot(ArrayList<Integer> allSevens) {
-
-        return false;
+        for(int number : allSevens) {
+            if (number != 7){
+                return false;
+            }
+        }
+        return true;
     }
 
     public int payOutForThree(boolean matchThreeTrue) {
-
-        return 0;
+        int payOutThree = matchThreeTrue ? 1 : 0;
+        return payOutThree * 3;
     }
 
     public int payOutForFive(boolean matchFiveTrue) {
-
-        return 0;
+        int payOutThree = matchFiveTrue ? 1 : 0;
+        return payOutThree * 7;
     }
 
-
-    public int payOutForJackpot(boolean JackpotTrue) {
-
-        return 0;
+    public int payOutForJackpot(boolean jackpotTrue) {
+        int payOutThree = jackpotTrue ? 1 : 0;
+        return payOutThree * 20;
     }
 }
