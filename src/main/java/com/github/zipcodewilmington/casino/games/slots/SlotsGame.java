78b61de050f9ee9695cfb57d30f231ef61770772
ttxlnguyen <ttxl.nguyen@gmail.com>
2024-07-19 15:10:03 -0400

    Made more scenario test cases, finished looping logic, game currently runs, will come back to make better

--- src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/slots/SlotsGame.java
@@ -1,21 +1,53 @@
 package com.github.zipcodewilmington.casino.games.slots;
 
 
-import java.util.ArrayList;
-import java.util.Random;
+import java.util.*;
 
 
 /**
  * Created by leon on 7/21/2020.
  */
 public class SlotsGame {
+    Scanner scanner = new Scanner(System.in);
+    int wagerAmount;
+    ArrayList<Integer> randomList;
 
     public static void main(String[] args) {
+        int startGambling = 0;
 
+        while (startGambling == 0) {
+            SlotsGame slots = new SlotsGame();
+            slots.placeWager(slots.wagerAmount);
 
+            if (slots.checkForjackpot(slots.randomList)){
+                slots.payOutForJackpot(100, slots.wagerAmount);
+                System.out.println("You got paid: " + slots.payOutForJackpot(0, slots.wagerAmount));
+                if (slots.pullLever() == false) {
+                    startGambling = 1;
+                }
+            } else if (slots.checkForMatchFive(slots.randomList)) {
+                slots.payOutForFive(40, slots.wagerAmount);
+                System.out.println("You got paid: " + slots.payOutForFive(0, slots.wagerAmount));
+                if (slots.pullLever() == false) {
+                    startGambling = 1;
+                }
+
+            } else if (slots.checkForMatchThree(slots.randomList)) {
+                slots.payOutForThree(15, slots.wagerAmount);
+                System.out.println("You got paid: " + slots.payOutForThree(0, slots.wagerAmount));
+                if (slots.pullLever() == false) {
+                    startGambling = 1;
+
+                }
+            }
+        }
     }
 
-    public ArrayList<Integer> spinWheel(boolean isLeverPulled, int size) {
+    /*===================================
+     UML
+     ===================================*/
+
+    public ArrayList<Integer> spinWheelYes(boolean isLeverPulled, int size) {
         ArrayList<Integer> newArray = new ArrayList<>();
         Random random = new Random();
 
@@ -24,45 +56,62 @@ public class SlotsGame {
             for (int i = 0; i < size; i++){
                 newArray.add(random.nextInt(10));
             }
+            System.out.println(newArray);
         }
+        this.randomList = newArray;
         return newArray;
     }
 
     public boolean checkForMatchThree(ArrayList<Integer> matchThree) {
-        //creates an array 'duplicate' from 0-9
-        int[] duplicate = new int[10];
-        //counts occurences of each number in the array 'duplicate'
-        for (int number : matchThree) {
-            duplicate[number]++;
-        }
-        //iterates through the 'duplicate' array if any number has a value of 3 or 4 return true
-        for (int i = 0; i < 10; i++) {
-            if (duplicate[i] == 3 ) {
-                return true;
-            } else if (duplicate[i] == 4) {
-                return true;
+        int maxCount = 0;
+//        int mostFrequent = 0;
+
+        for (int i = 0; i < matchThree.size(); i++){
+            int count = 0;
+            //checks each element to see how often it appears
+            //then adds it to the mostFrequent if it beats
+            for (int j = 0; j < matchThree.size(); j++){
+                if (matchThree.get(i) == matchThree.get(j)){
+                    count++;
+                }
+            }
+            if (count > maxCount){
+                maxCount = count;
+//                mostFrequent = matchThree.get(i);
             }
         }
-        return false;
+        if (maxCount < 5 && maxCount >=3){
+            System.out.println("You matched 3!");
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public boolean checkForMatchFive(ArrayList<Integer> matchFive) {
-        //creates an array 'duplicate' from 0-9
-        int[] duplicate = new int[10];
-        //counts occurences of each number in the array 'duplicate'
-        for (int number : matchFive) {
-            duplicate[number]++;
-        }
-        //iterates through the 'duplicate' array if any number has a value of 5
-        for (int i = 0; i < 10; i++) {
-            if (duplicate[i] == 5) {
-                return true;
+        int maxCount = 0;
+
+        for (int i = 0; i < matchFive.size(); i++){
+            int count = 0;
+
+            for (int j = 0; j < matchFive.size(); j++){
+                if (matchFive.get(i) == matchFive.get(j)){
+                    count++;
+                }
+            }
+            if (count > maxCount){
+                maxCount = count;
             }
         }
-        return false;
+
+        if (maxCount == 5){
+            return true;
+        } else {
+            return false;
+        }
     }
 
-    public boolean jackpot(ArrayList<Integer> allSevens) {
+    public boolean checkForjackpot(ArrayList<Integer> allSevens) {
         for(int number : allSevens) {
             if (number != 7){
                 return false;
@@ -71,18 +120,85 @@ public class SlotsGame {
         return true;
     }
 
-    public int payOutForThree(boolean matchThreeTrue) {
-        int payOutThree = matchThreeTrue ? 1 : 0;
-        return payOutThree * 3;
+    public int payOutForThree(int payOutThree, int wagerAmount) {
+        payOutThree = 15;
+        int payout = payOutThree * wagerAmount;
+        return payout;
     }
 
-    public int payOutForFive(boolean matchFiveTrue) {
-        int payOutThree = matchFiveTrue ? 1 : 0;
-        return payOutThree * 7;
+    public int payOutForFive(int payOutFive, int wagerAmount) {
+        payOutFive = 100;
+        int payout = payOutFive * wagerAmount;
+        return payout;
     }
 
-    public int payOutForJackpot(boolean jackpotTrue) {
-        int payOutThree = jackpotTrue ? 1 : 0;
-        return payOutThree * 20;
+    public int payOutForJackpot(int payOutJackpot, int wagerAmount) {
+        payOutJackpot = 1000;
+        int payout = payOutJackpot * wagerAmount;
+        return payout;
+    }
+
+    /*===================================
+    NEW METHODS
+    ===================================*/
+
+    public void placeWager(int bet) {
+        System.out.print("How much would you like to bet? 5, 10, 20, 50, 100, 250: ");
+        bet = 0;
+        while (true) {
+            bet = scanner.nextInt();
+            switch (bet) {
+                case 5:
+                    this.wagerAmount = bet;
+                    System.out.println("You've bet: 5");
+                    this.spinWheelYes(true, 5);
+                    return;
+                case 10:
+                    this.wagerAmount = bet;
+                    System.out.println("You've bet: 10");
+                    this.spinWheelYes(true, 5);
+                    return;
+                case 20:
+                    this.wagerAmount = bet;
+                    System.out.println("You've bet: 20");
+                    this.spinWheelYes(true, 5);
+                    return;
+                case 50:
+                    this.wagerAmount = bet;
+                    System.out.println("You've bet: 50");
+                    this.spinWheelYes(true, 5);
+                    return;
+                case 100:
+                    this.wagerAmount = bet;
+                    System.out.println("You've bet: 100");
+                    this.spinWheelYes(true, 5);
+                    return;
+                case 250:
+                    this.wagerAmount = bet;
+                    System.out.println("You've bet: 250");
+                    this.spinWheelYes(true, 5);
+                    return;
+                default:
+                    System.out.println("That's not an option." +
+                            "\nHow much would you like to bet? 5, 10, 20, 50, 100, 250");
+                }
+            }
+        }
+
+
+    public boolean pullLever() {
+        System.out.println("Would you like to continue? [Yes] [No]");
+
+        while (true) {
+            String choice = scanner.nextLine();
+            switch (choice) {
+                case "yes":
+                    return true;
+                case "no":
+                    return false;
+                default:
+                    System.out.println("That's not an option. Would you like to continue? [Yes] [No]");
+            }
+        }
     }
 }
--- src/test/java/com/github/zipcodewilmington/SlotsTest.java
+++ src/test/java/com/github/zipcodewilmington/SlotsTest.java
@@ -8,6 +8,8 @@ import org.testng.Assert;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import static org.junit.Assert.assertEquals;
+
 public class SlotsTest {
 
     SlotsGame slots;
@@ -21,92 +23,108 @@ public class SlotsTest {
     public final void testSpinWheelTrue(){
         //given boolean
         //return arraylist
-        SlotsGame slotsGame = new SlotsGame();
         boolean decision = true;
         //calls spinWheel method to generate an arrayList of random numbers of size 5
-        ArrayList<Integer> arrayList = slotsGame.spinWheel(decision, 5);
+        ArrayList<Integer> arrayList = slots.spinWheelYes(decision, 5);
         Assert.assertEquals(arrayList.size(), 5);
     }
 
     @Test
     public final void testCheckForMatchThree(){
         //given an arraylist of size 5 with random numbers from 0-9 check if there are 3 matching numbers
-        SlotsGame slotsGame = new SlotsGame();
-        //creates an ArrayList with random numbers but using 3 matching for testing purposes
-        ArrayList<Integer> matchThree = new ArrayList<>(Arrays.asList(1, 3, 5, 5, 5));
-        ArrayList<Integer> noMatch = new ArrayList<>(Arrays.asList(1,3,4,6,5));
-        //return boolean
-        Assert.assertTrue(slotsGame.checkForMatchThree(matchThree));
-        Assert.assertFalse(slotsGame.checkForMatchThree(noMatch));
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1,1,1,4,5));
+        boolean result = slots.checkForMatchThree(arrayList);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public final void testCheckForMatchThree2(){
+        //given an arraylist of size 5 with random numbers from 0-9 check if there are 3 matching numbers
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1,1,3,4,5));
+        boolean result = slots.checkForMatchThree(arrayList);
+        Assert.assertFalse(result);
     }
 
     @Test
     public final void testCheckForMatchFive(){
         //given an arraylist of size 5 with random numbers from 0-9 check if there are 5 matching numbers
-        SlotsGame slotsGame = new SlotsGame();
-        //creates an ArrayList with random numbers but using all 5's for testing purposes
-        ArrayList<Integer> matchThree = new ArrayList<>(Arrays.asList(5, 5, 5, 5, 5));
-        ArrayList<Integer> noMatch = new ArrayList<>(Arrays.asList(1,3,4,6,5));
-        //return boolean
-        Assert.assertTrue(slotsGame.checkForMatchFive(matchThree));
-        Assert.assertFalse(slotsGame.checkForMatchFive(noMatch));
+        //creates an ArrayList with random numbers but using all 1's for testing purposes
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1, 1, 1, 1, 1));
+        boolean result = slots.checkForMatchFive(arrayList);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public final void testCheckForMatchFive2(){
+        //given an arraylist of size 5 with random numbers from 0-9 check if there are 5 matching numbers
+        //creates an ArrayList with random numbers but using all 1's for testing purposes
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(1, 1, 1, 1, 5));
+        boolean result = slots.checkForMatchFive(arrayList);
+        Assert.assertFalse(result);
     }
 
     @Test
     public final void testJackpot(){
         //given an arraylist of size 5 with all elements = 7
-        SlotsGame slotsGame = new SlotsGame();
         //creates an ArrayList with random numbers but using all 7's for testing purposes
         ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(7, 7, 7, 7, 7));
-        boolean condition = slotsGame.jackpot(arrayList);
+        boolean condition = slots.checkForjackpot(arrayList);
         //return boolean
         Assert.assertTrue(condition, "All numbers in ArrayList are 7's");
     }
 
+    @Test
+    public final void testJackpot2(){
+        //given an arraylist of size 5 with all elements = 7
+        //creates an ArrayList with random numbers but using all 7's for testing purposes
+        ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(5, 5, 5, 5, 5));
+        boolean condition = slots.checkForjackpot(arrayList);
+        //return boolean
+        Assert.assertFalse(condition, "All numbers in ArrayList are 5's");
+    }
 
     @Test
     public final void payOutFor3Test(){
-        //given a boolean, return int times three
-        SlotsGame slotsGame = new SlotsGame();
+        //return int times 15
 
-        //returns int times three
-        boolean resultTrue = true;
-        int conditionTrue = slotsGame.payOutForThree(resultTrue);
-        Assert.assertEquals(3, conditionTrue);
+        double results = slots.payOutForThree(15, 1);
+        Assert.assertEquals(results, 15);
+    }
 
-        boolean resultFalse = false;
-        int conditionFalse = slotsGame.payOutForThree(resultFalse);
-        Assert.assertEquals(0, conditionFalse);
+    @Test
+    public final void payOutFor3Test2(){
+        //return int times 15
+
+        double results = slots.payOutForThree(15, 5);
+        Assert.assertEquals(results, 75);
     }
 
     @Test
     public final void payOutFor5Test(){
-        //given a boolean, return int times seven
-        SlotsGame slotsGame = new SlotsGame();
+        //return int times 40
+        double results = slots.payOutForFive(100, 1);
+        Assert.assertEquals(results, 100);
+    }
 
-        //return int times seven
-        boolean resultTrue = true;
-        int conditionTrue = slotsGame.payOutForFive(resultTrue);
-        Assert.assertEquals(7, conditionTrue);
-
-        boolean resultFalse = false;
-        int conditionFalse = slotsGame.payOutForFive(resultFalse);
-        Assert.assertEquals(0, conditionFalse);
+    @Test
+    public final void payOutFor5Test2(){
+        //return int times 40
+        double results = slots.payOutForFive(100, 5);
+        Assert.assertEquals(results, 500);
     }
 
     @Test
     public final void payOutJackpot(){
-        //given a boolean, return int times 20
-        SlotsGame slotsGame = new SlotsGame();
+        //given a boolean, return int times 100
+        double results = slots.payOutForJackpot(1000, 1);
+        Assert.assertEquals(results, 1000);
+    }
 
-        boolean resultTrue = true;
-        int conditionTrue = slotsGame.payOutForJackpot(resultTrue);
-        Assert.assertEquals(20, conditionTrue);
-
-        boolean resultFalse = false;
-        int conditionFalse = slotsGame.payOutForJackpot(resultFalse);
-        Assert.assertEquals(0, conditionFalse);
-
+    @Test
+    public final void payOutJackpot2(){
+        //given a boolean, return int times 100
+        double results = slots.payOutForJackpot(1000, 5);
+        Assert.assertEquals(results, 5000);
     }
 
 }
