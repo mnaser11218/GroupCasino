bc94be4ffab0a729dd9b38a5dd0901b68ed8e7c9
Devi <diptikadevi@gmail.com>
2024-07-19 17:14:09 -0400

    roulette is talking to casino

--- src/main/java/com/github/zipcodewilmington/casino/games/roulette/RoulettePlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/roulette/RoulettePlayer.java
@@ -1,4 +1,12 @@
 package com.github.zipcodewilmington.casino.games.roulette;
 
-public class RoulettePlayer {
+import com.github.zipcodewilmington.casino.CasinoAccount;
+import com.github.zipcodewilmington.casino.CasinoAccountManager;
+import com.github.zipcodewilmington.casino.PlayerInterface;
+
+public class RoulettePlayer extends CasinoAccountManager implements PlayerInterface {
+    @Override
+    public CasinoAccount getArcadeAccount(String userName, String password) {
+        return getAccount(userName, password);
+    }
 }
