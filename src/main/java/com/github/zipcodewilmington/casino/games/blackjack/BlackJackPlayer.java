ad31773e8a6af35c49070e65272ddf50f1c90489
Josue <josue@Zipcoders-MacBook-Pro-75.local>
2024-07-19 17:09:13 -0400

    blackjackIsRunningHooray

--- src/main/java/com/github/zipcodewilmington/casino/games/blackjack/BlackJackPlayer.java
+++ src/main/java/com/github/zipcodewilmington/casino/games/blackjack/BlackJackPlayer.java
@@ -1,4 +1,15 @@
 package com.github.zipcodewilmington.casino.games.blackjack;
 
-public class BlackJackPlayer {
+import com.github.zipcodewilmington.casino.CasinoAccount;
+import com.github.zipcodewilmington.casino.CasinoAccountManager;
+import com.github.zipcodewilmington.casino.PlayerInterface;
+
+public class BlackJackPlayer extends CasinoAccountManager implements PlayerInterface {
+
+
+    @Override
+    public CasinoAccount getArcadeAccount(String userName, String password) {
+        return getAccount(userName, password);
+
+    }
 }
